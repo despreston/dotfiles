2c0b1608797eda5d338402e863ad923376f8ab51
Des Preston <despreston@gmail.com>
2021-01-26 15:55:53 -0500

    nix darwin config

---
+++ .nixpkgs/darwin-configuration.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+{
+  # system packages to install
+  environment.systemPackages = with pkgs; [
+    gitAndTools.gitFull
+    tmux
+    vim
+    jq
+    oh-my-zsh
+    go
+    mongodb
+    redis
+    awscli2
+  ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  # services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;
+  programs.zsh.promptInit = "";
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
