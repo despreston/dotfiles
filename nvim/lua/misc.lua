5b3950975e73cc413860fb57b6e27de1a04bbff2
Des Preston <despreston@gmail.com>
2021-12-14 18:52:10 -0500

    dont need so much info, lualine

--- nvim/lua/misc.lua
+++ nvim/lua/misc.lua
@@ -22,13 +22,15 @@ require('lualine').setup{
     theme = 'gruvbox',
   },
   sections = {
+    lualine_b = {'branch'},
     lualine_c = {
       {
         'filename',
         file_status = true, -- displays file status (readonly status, modified status)
         path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
       }
-    }
+    },
+    lualine_x = {'filetype'},
   }
 }
 
